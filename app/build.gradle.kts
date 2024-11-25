148a204f4fb13b0ec81c7b385c9028fd34a493b2
Wesly <siallaganwesly@gmail.com>
2024-11-25 22:01:41 +0700

    Logcat login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
--- app/src/main/java/com/example/simple_login_page/MainActivity.kt
+++ app/src/main/java/com/example/simple_login_page/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.simple_login_page
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var username: EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        loginButton = findViewById(R.id.buttonLogin)
+
+        loginButton.setOnClickListener{
+            val usernamelogin = username.text.toString()
+            val passwordlogin = password.text.toString()
+            Log.i("Testing Login", "Username : $usernamelogin and password : $passwordlogin")
         }
+
     }
 }
